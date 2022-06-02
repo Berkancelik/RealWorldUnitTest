dc12fbf0089eee8d74eb412206d38c8e68136b9f
Berkan Çelik <79005929+Berkancelik@users.noreply.github.com>
2022-06-02 09:46:59 +0300

    ProductControllerTest fixed

---
+++ RealWorldUnitTest.Test/ProductControllerTest.cs
@@ -0,0 +1,28 @@
+﻿using Moq;
+using RealWorldUnitTest.Web.Controllers;
+using RealWorldUnitTest.Web.Models;
+using RealWorldUnitTest.Web.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RealWorldUnitTest.Test
+{
+
+    public class ProductControllerTest
+    {
+        private readonly Mock<IRepository<Product>> _mockRepo;
+        private readonly ProductsController _controller;
+        private List<Product> products;
+
+        public ProductControllerTest()
+        {
+            _mockRepo = new Mock<IRepository<Product>>();
+            _controller = new ProductsController(_mockRepo.Object);
+            products = new List<Product>() { new Product { Id = 1, Name = "Kalem", Price = 500, Stock = 54, Color = "Mavi" },
+                new Product { Id = 2, Name = "Defter", Price = 450, Stock = 22, Color = "Turuncu" } };
+        }
+    }
+}
