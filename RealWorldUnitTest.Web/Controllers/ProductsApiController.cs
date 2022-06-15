42a62cdc5ee33b06fe3d6c07cfae4fdcfed7b07b
Berkan Çelik <79005929+Berkancelik@users.noreply.github.com>
2022-06-15 10:32:44 +0300

    ProductsApi Controller has been created

---
+++ RealWorldUnitTest.Web/Controllers/ProductsApiController.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RealWorldUnitTest.Web.Models;
+using RealWorldUnitTest.Web.Repository;
+
+namespace RealWorldUnitTest.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsApiController : ControllerBase
+    {
+        private readonly IRepository<Product> _repository;
+
+        public ProductsApiController(IRepository<Product> repository)
+        {
+            _repository = repository;
+        }
+
+
+        // GET: api/ProductsApi
+        [HttpGet]
+        public async Task<IActionResult> GetProducts()
+        {
+            var product = await _repository.GetAll();
+            return Ok(product);
+
+        }
+
+        // GET: api/ProductsApi/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetProduct(int id)
+        {
+            var product = await _repository.GetById(id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(product);
+        }
+
+        // PUT: api/ProductsApi/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public IActionResult PutProduct(int id, Product product)
+        {
+            if (id != product.Id)
+            {
+                return BadRequest();
+            }
+
+            _repository.Update(product);
+
+
+            return NoContent();
+        }
+
+        // POST: api/ProductsApi
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<IActionResult> PostProduct(Product product)
+        {
+            await _repository.Create(product);
+            //201 status code
+            return CreatedAtAction("GetProduct", new { id = product.Id }, product);
+        }
+
+        // DELETE: api/ProductsApi/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteProduct(int id)
+        {
+            var product = await _repository.GetById(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            _repository.Update(product);
+
+
+            return NoContent();
+        }
+
+        private bool ProductExists(int id)
+        {
+            Product product = _repository.GetById(id).Result;
+            if (product == null)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+    }
+}
